512b179a677edfbbeed09d6ae4cb59e83ba81329
terence <terencematsune11@gmail.com>
2025-07-10 19:06:06 -0700

    Feature: 9600 baud rate UART peripheral

--- src/drivers/uart.c
+++ src/drivers/uart.c
@@ -1,61 +1,37 @@
 #include "uart.h"
-#include "common/defines.h"
 #include <msp430.h>
-#include <assert.h> 
-#include <stdint.h> 
-#include <stddef.h>
-
-#define UART_BUFFER_SIZE (16u)
-// Configuring the UART hardware registers
-#define BRCLK (SMCLK) // system clock 16MHz
-#define UART_BAUD_RATE (115200u) // Baud rate 
-static_assert(UART_BAUD_RATE < (BRCLK / 3.0f), "Baudrate must be smaller than 1/3 of input clock in Low-Frequency Mode");
-// MSP430 need clock divisor how much to divide SMCLK to get right baud rate
-#define UART_DIVISOR ((float)BRCLK / UART_BAUD_RATE) //  (e.g. 16MHz / 115200) ~138.888
-static_assert(UART_DIVISOR < 0xFFFFu, "Sanity check divisor fits in 16-bit");
-#define UART_DIVISOR_INT_16BIT ((uint16_t)UART_DIVISOR)
-#define UART_DIVISOR_INT_LOW_BYTE (UART_DIVISOR_INT_16BIT & 0xFF)
-#define UART_DIVISOR_INT_HIGH_BYTE (UART_DIVISOR_INT_16BIT >> 8)
-#define UART_DIVISOR_FRACTIONAL (UART_DIVISOR - UART_DIVISOR_INT_16BIT)
-#define UART_UCBRS ((uint8_t)(8 * UART_DIVISOR_FRACTIONAL))
-#define UART_UCBRF (0)
-#define UART_UC0S16 (0)
-static_assert(UART_UCBRS < 8, "Sanity check second modulation stage value fits in 3-bit");
+#include <stdint.h>
 
 void uart_init(void)
 {
-  /* Reset module. It stays in reset until cleared. The module should be in reset
-  * condition while configured according to the user guide (SLAU144K). */
+  // Hold USCI in reset during config
   UCA0CTL1 |= UCSWRST;
 
-  /* Use default (data word length 8 bits, 1 stop bit, no parity bit)
-  * [ Start (1 bit) | Data (8 bits) | Stop (1 bit) ] */
-  UCA0CTL0 = 0;
-
-  // Set SMCLK as clock source.
+  // Use SMCLK (16 MHz)
   UCA0CTL1 |= UCSSEL_2;
 
-  // Set clock prescaler to the integer part of divisor N.
-  UCA0BR0 = UART_DIVISOR_INT_LOW_BYTE;
-  UCA0BR1 = UART_DIVISOR_INT_HIGH_BYTE;
+  // Configure TXD/RXD pins (P1.1 = RX, P1.2 = TX)
+  P1SEL |= BIT1 | BIT2;
+  P1SEL2 |= BIT1 | BIT2;
 
-  /* Set modulation to account for the fractional part of divisor N.
-  * UCA0MCTL = [UCBRF (4 bits) | UCBRS (3 bits) | UC0S16 (1 bit) ] */
-  UCA0MCTL = (UART_UCBRF << 4) + (UART_UCBRS << 1) + UART_UC0S16;
+  // For 16 MHz and 9600 baud:
+  // BR = 1667 (0x0683), modulation = UCBRS_0 + UCBRF_0 + UCOS16 = 0
+  UCA0BR0 = 0x83;     // low byte = 131
+  UCA0BR1 = 0x06;     // high byte = 6
+  UCA0MCTL = UCBRS_0; // No modulation
 
-  // Clear reset to release the module for operation.
+  // Enable USCI
   UCA0CTL1 &= ~UCSWRST;
-};
+}
 
 void uart_putchar_pulling(char c)
 {
-  while(!(IFG2 & UCA0TXIFG));
+  while (!(IFG2 & UCA0TXIFG));
   UCA0TXBUF = c;
 
-  if (c == '\n'){
-    uart_putchar_pulling('\r');
-  } 
+  if (c == '\n') {
+    while (!(IFG2 & UCA0TXIFG));
+    UCA0TXBUF = '\r';
+  }
 }
-
-
 
